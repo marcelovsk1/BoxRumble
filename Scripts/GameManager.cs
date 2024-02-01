11d338cad5e7b77c4f29514a780a8f0abc3c487e
Marcelo Amaral Alves <marceloaa@live.com>
2024-02-01 15:20:33 -0300

    working on gameManager

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject hazardPrefab;
+    // Start is called before the first frame update
+    public int maxHazardsToSpawn = 3;
+    public TMPro.TextMeshProUGUI scoreText;
+    public Image backgroundMenu;
+
+    private int score;
+    private float timer;
+    private static bool gameOver;
+
+    void Start()
+    {
+        StartCoroutine(SpawnHazards());
+    }
+
+    private void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.Escape))
+      {
+       if (Time.timeScale == 0)
+       {
+          StartCoroutine(ScaleTime(0, 1, 0.5f));
+          backgroundMenu.gameObject.SetActive(false);
+       }
+       if (Time.timeScale == 1)
+       {
+          StartCoroutine(ScaleTime(1, 0, 0.5f));
+          backgroundMenu.gameObject.SetActive(true);
+       }
+      }
+
+      if (gameOver)
+        return;
+      timer += Time.deltaTime;
+
+      if(timer >= 1f)
+      {
+        score++;
+        scoreText.text = score.ToString();
+
+        timer = 0;
+      }
+    }
+
+    IEnumerator ScaleTime(float start, float end, float duration)
+    {
+      float lastTime = Time.realtimeSinceStartup;
+      float timer = 0.0f;
+
+      while (timer < duration)
+      {
+        Time.timeScale = Mathf.Lerp(start, end, timer / duration);
+        Time.fixedDeltaTime = 0.02f * Time.timeScale;
+        timer += (Time.realtimeSinceStartup - lastTime);
+        lastTime = Time.realtimeSinceStartup;
+        yield return null;
+      }
+
+      Time.timeScale = end;
+      Time.fixedDeltaTime = 0.02f * Time.timeScale;
+    }
+
+    private IEnumerator SpawnHazards()
+    {
+      var hazardToSpawn = Random.Range(1, 4);
+
+      for (int i = 0; i < hazardToSpawn; i++)
+    {
+      var x = Random.Range(-11 ,11);
+      var drag = Random.Range(1f, 3f);
+
+      var hazard = Instantiate(hazardPrefab, new Vector3(x, 11, 0), Quaternion.identity); //Hazard Height
+      hazard.GetComponent<Rigidbody>().drag = drag;
+    }
+
+      yield return new WaitForSeconds(1.5f);
+
+      yield return SpawnHazards();
+
+    }
+
+    public static void GameOver()
+    {
+      gameOver = true;
+    }
+
+}
