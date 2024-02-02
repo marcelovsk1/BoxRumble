5704148b4b53187094cc41e9fdc66615e7842f6c
Marcelo Amaral Alves <marceloaa@live.com>
2024-02-02 16:59:08 -0300

    working on autodistoryer

--- Scripts/Extras/AutoDestroyer.cs
+++ Scripts/Extras/AutoDestroyer.cs
@@ -6,7 +6,6 @@ public class AutoDestroy : MonoBehaviour
 {
 
   public float delay;
-    // Start is called before the first frame update
     void Start()
     {
         Destroy(gameObject, delay);
